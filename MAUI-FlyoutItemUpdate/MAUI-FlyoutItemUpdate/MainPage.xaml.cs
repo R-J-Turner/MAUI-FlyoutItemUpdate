9fb1426974a461549b994737ee5455d2b56f44b2
R-J-Turner <128035123+R-J-Turner@users.noreply.github.com>
2023-03-16 10:43:19 +0000

    init

---
+++ MAUI-FlyoutItemUpdate/MAUI-FlyoutItemUpdate/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MAUI_FlyoutItemUpdate;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage() { InitializeComponent(); }
+
+    private void AddBtn_Clicked(object sender, EventArgs e)
+    {
+        Microsoft.Maui.Controls.ShellItem item = new Microsoft.Maui.Controls.ShellItem()
+        {
+            Title = "New Item",
+            Route = "MainPage"
+        };
+
+        Shell.Current.Items.Add(this);
+    }
+
+    private void ShowBtn_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.Items[2].IsEnabled = true;
+        Shell.Current.Items[2].IsVisible = true;
+        Shell.Current.Items[2].FlyoutItemIsVisible = true;
+    }
+}
+
